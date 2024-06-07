3accd1bf2d492e522456c2cc09eaa34174e36078
Karel Kroeze <k.a.kroeze@utwente.nl>
2024-06-07 13:32:22 +0200

    update for new people API, add way to mark authors external, add sanity check on result, stop storing all results in local storage

--- assets/js/pages/people.js
+++ assets/js/pages/people.js
@@ -1,4 +1,7 @@
 
+const token="c2VjcmV0LXRva2VuOjcwNWE0OTQyNGE3OTRiNTM1Mzc2MzE2YzQ2NGI1OTYzNzI3NTcwNzc1NjUx"
+const localStorageKey="bdsi.people"
+const localStorageKeyVersion=2
 
 /**
  * Call the UTwente people API to attempt to resolve non-BDSi people mentioned in projects, workshops, etc.
@@ -7,22 +10,34 @@
  * @returns {{ found: number, data: any[]}}
  */
 const resolve = async name => {
-    let data = localStorage.getItem(`bdsi.people.${name}`);
-    if (data) {
-        data = JSON.parse(data);
-        console.debug({ name, data }, "resolved from cache");
-    } else {
-        const response = await fetch(`https://people.utwente.nl/peoplepagesopenapi/contacts?query=${name}`);
-        data = await response.json();
-        if (data.found) {
-            localStorage.setItem(`bdsi.people.${name}`, JSON.stringify(data));
-            console.debug({ name, data }, "resolved from people API");
-        } else {
-            console.log({ name }, "could not be resolved")
-        }
-    }
+    // try resolve from browser storage
+    let person = localStorage.getItem(`${localStorageKey}.v${localStorageKeyVersion}.${name}`);
+    if (person) {
+        person = JSON.parse(person);
+        console.debug({ name, data: person }, "resolved from cache");
+        return person;
+    } 
 
-    return data;
+    // try resolve from people API
+    const response = await fetch(`https://people.utwente.nl/peoplepagesopenapi/contacts?authorization=${atob(token)}&query=${name}`);
+    data = await response.json();
+    if (data.found) {
+        // console.log({name, data});
+
+        // consider first result only
+        person = data.data[0]
+        // console.log({person})
+
+        // check for at least a partial match
+        if (stringSimilarity(name, person.givenName + " " + person.displayName.split(",", 1).join("")) >= 0.5) {
+            localStorage.setItem(`${localStorageKey}.v${localStorageKeyVersion}.${name}`, JSON.stringify(person));
+            console.debug({ name, person }, "resolved from people API");
+            return person;
+        }
+    } 
+
+    // we failed...
+    console.debug({ name, person, data }, "could not be resolved")
 }
 
 /**
@@ -33,10 +48,10 @@ const resolveAll = async () => {
     * @type {IterableIterator<HTMLDivElement>} 
     * */
     const people = document.querySelectorAll(".author-list-item.unresolved").values();
-    for (const person of people) {
-        const data = await resolve(person.dataset["name"])
-        if (data.found) {
-            hydrate(data.data[0], person)
+    for (const personElement of people) {
+        const person = await resolve(personElement.dataset["name"])
+        if (person){ 
+            hydrate(person, personElement)
         }
     }
 }
@@ -63,11 +78,52 @@ const hydrate = (person, element) => {
     anchor.append(element);
 
     // create and insert profile image
-    const image = document.createElement("img");
-    image.src = person.pictureUrl;
-    image.alt = person.displayName;
+    if (person.pictureUrl) {
+        const image = document.createElement("img");
+        image.src = person.pictureUrl;
+        image.alt = person.displayName;
 
-    element.prepend(image)
+        element.prepend(image);
+    }
 }
+
+/**
+ * Calculate similarity between two strings
+ * 
+ * Copied from https://github.com/stephenjjbrown/string-similarity-js.
+ * @license MIT
+ * @param {string} str1 First string to match
+ * @param {string} str2 Second string to match
+ * @param {number} [substringLength=2] Optional. Length of substring to be used in calculating similarity. Default 2.
+ * @param {boolean} [caseSensitive=false] Optional. Whether you want to consider case in string matching. Default false;
+ * @returns Number between 0 and 1, with 0 being a low match score.
+ */
+var stringSimilarity = function (str1, str2, substringLength, caseSensitive) {
+    if (substringLength === void 0) { substringLength = 2; }
+    if (caseSensitive === void 0) { caseSensitive = false; }
+    if (!caseSensitive) {
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+    }
+    if (str1.length < substringLength || str2.length < substringLength)
+        return 0;
+    var map = new Map();
+    for (var i = 0; i < str1.length - (substringLength - 1); i++) {
+        var substr1 = str1.substr(i, substringLength);
+        map.set(substr1, map.has(substr1) ? map.get(substr1) + 1 : 1);
+    }
+    var match = 0;
+    for (var j = 0; j < str2.length - (substringLength - 1); j++) {
+        var substr2 = str2.substr(j, substringLength);
+        var count = map.has(substr2) ? map.get(substr2) : 0;
+        if (count > 0) {
+            map.set(substr2, count - 1);
+            match++;
+        }
+    }
+    return (match * 2) / (str1.length + str2.length - ((substringLength - 1) * 2));
+};
+
+
 
 resolveAll()
